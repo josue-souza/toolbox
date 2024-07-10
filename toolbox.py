a62619412a70e446914ff1be838dbb0ec3a98610
josue.souza <josue.souza@fgv.br>
2024-07-10 10:15:45 -0700

    Correção no menu para editar as tarefas

--- toolbox.py
+++ toolbox.py
@@ -1,13 +1,12 @@
 import requests
 from datetime import datetime
 import json
-from firebase_admin import db
 
 class Toolbox():
-    def __init__(self):
-        self.url_firebase = '###'
-        self.url_firebase_json = '###'
+    def __init__(self, url_firebase, key_weatherbit):
+        self.url_firebase_json = url_firebase
         self.url_weatherbit = 'https://api.weatherbit.io/v2.0/current'
+        self.key_weatherbit = key_weatherbit
 
     def request_api(self, url, method, str_json, str_data, str_params):
         if method.lower() in ['get', 'post', 'put', 'delete', 'patch']:
@@ -16,7 +15,8 @@ class Toolbox():
 
 
     def post_to_do(self, date, name, task):
-        json_data = {date: {'nome': name, 'tarefa': task}}
+        json_data = {'data': date, 'nome': name, 'tarefa': task, 'status': 'não definido'}
+        print(name)
         response = self.request_api(self.url_firebase_json, 'post', json_data, None, None)
         if response.status_code == 200:
             print('Tarefa criada com sucesso! \n')
@@ -30,17 +30,34 @@ class Toolbox():
         response = self.request_api(self.url_firebase_json, 'get', None, None, None)
         date_now = response.json()
         api_return = []
-        for value in date_now.values():
-            for item_key, item_value in value.items():
-                if item_key == date:
-                    name = item_value.get('nome')
-                    task = item_value.get('tarefa') 
-                    status = item_value.get('status')
+        try:
+            for value in date_now.values():
+                value_date = value.get('data')
+                if value_date == date:
+                    name = value.get('nome')
+                    task = value.get('tarefa') 
+                    status = value.get('status')
                     api_return.append((date, name, task, status))
-                    
-        if not api_return:
-            print('Não há tarefas para esta data!')
-        return api_return
+                        
+            if not api_return:
+                print('Não há tarefas para esta data!')
+            return api_return
+        except AttributeError:
+            print('Nenhuma tarefa foi adicionada!')
+    
+    def list_all_to_do(self):
+        response = self.request_api(self.url_firebase_json, 'get', None, None, None)
+        data = response.json()
+        try:
+            for value in data.values():
+                date = value.get('data')
+                name = value.get('nome')
+                task = value.get('tarefa') 
+                status = value.get('status')
+                if status != 'realizado' and date != None and name != None:
+                    print(f"\tData: {date} - Nome: {name}")
+        except AttributeError:
+            print('Nenhuma tarefa foi adicionada!')
     
 
     def processing_data_to_do(self, task):
@@ -50,48 +67,22 @@ class Toolbox():
         key_node = None
         for key, valor_name in data.items():
             for data_key, valor_task in valor_name.items():
-                if 'tarefa' in valor_task and valor_task['tarefa'] == desc_task:
+                if valor_task == desc_task:
                     key_node = key
                     break
         return key_node
  
 
     def update_to_do(self, node, date, name, task, status):
-        if date != None:
-            params = {node: {'data': date}}
-            data = json.dumps(params)
-            response = self.request_api(self.url_firebase_json, 'patch', None, data, None)
-            if response.status_code == 200:
-                updated_data = response.json()
-                print('\n \tData alterada com sucesso!')
-            else:
-                print(f"Ocorreu um erro durante o processo. {response.status_code}")
-        if name != None:
-            params = {node: {'nome': name}}
-            data = json.dumps(params)
-            response = self.request_api(self.url_firebase_json, 'patch', None, data, None)            
-            if response.status_code == 200:
-                print('\n \tNome alterado com sucesso!')
-            else:
-                print(f"Ocorreu um erro durante o processo. {response.status_code}")
-
-        if task != None:
-            params = {node: {'tarefa': task}}
-            data = json.dumps(params)
-            response = self.request_api(self.url_firebase_json, 'patch', None, data, None)
-            if response.status_code == 200:
-                print('\n \tTarefa alterada com sucesso!')
-            else:
-                print(f"Ocorreu um erro durante o processo. {response.status_code}")
-        if status != None:
-            params = {node: {'status': status}}
-            data = json.dumps(params)
-            response = self.request_api(self.url_firebase_json, 'patch', None, data, None)
-            if response.status_code == 200:
-                print('\n \Status alterado com sucesso!')
-            else:
-                print(f"Ocorreu um erro durante o processo. {response.status_code}")
-        self.toolbox()
+        params = {'data': date, 'nome': name, 'tarefa': task, 'status': status}
+        data = json.dumps(params)
+        url = self.url_firebase_json.strip('.json')
+        response = self.request_api(f"{url}{node}.json", 'patch', None, data, None)
+        if response.status_code == 200:
+            updated_data = response.json()
+        else:
+            print(f"Ocorreu um erro durante o processo. {response.status_code}")
+        return response.status_code
 
 
     def menu_update_to_do(self, list_date):
@@ -121,29 +112,31 @@ class Toolbox():
                     if task_option == '1' or task_option.lower() == 'editar':
                         print('\n\t1 - Editar data\n\t2 - Editar nome\n\t3 - Editar tarefa\n\t4 - Editar status')
                         edit_option = input('Digite o número da opção desejada: ')
-                        
+                        date_start = selected_task[0]
+                        name_start = selected_task[1]
+                        status_start = selected_task[3]
+                        task_processing = selected_task[2]
                         if edit_option == '1' or edit_option.lower() == 'data':
                             edit_date = input('Digite a nova data (dd-mm-aaaa): ')
-                            date_start = selected_task[0]
-                            name_start = selected_task[1]
-                            status_start = selected_task[3]
-                            task_processing = selected_task[2]
                             processing_node = self.processing_data_to_do(task_processing)
-                            update = self.update_to_do(processing_node, edit_date, None, None, None)
-
+                            update = self.update_to_do(processing_node, edit_date, name_start, task_processing, status_start)
+                            if update == 200:
+                                print(f"\nData atualizada para {edit_date}")
                         elif edit_option == '2' or edit_option.lower() == 'nome':
                             edit_name = input('Digite o novo nome: ')
                             task_processing = selected_task[2]
                             processing_node = self.processing_data_to_do(task_processing)
-                            update = self.update_to_do(processing_node, None, edit_name, None, None)
-                            print(f"\nNome atualizado para {edit_name}")
+                            update = self.update_to_do(processing_node, date_start, edit_name, task_processing, status_start)
+                            if update == 200:
+                                print(f"\nNome atualizado para {edit_name}")
 
                         elif edit_option == '3' or edit_option.lower() == 'tarefa':
                             edit_task = input('Digite a nova tarefa: ')
                             task_processing = selected_task[2]
                             processing_node = self.processing_data_to_do(task_processing)
-                            update = self.update_to_do(processing_node, None, None, edit_task, None)
-                            print(f"\nTarefa atualizada para {edit_task}")
+                            update = self.update_to_do(processing_node, date_start, name_start, edit_task, status_start)
+                            if update == 200:
+                                print(f"\nTarefa atualizada para {edit_task}")
 
                         elif edit_option == '4' or edit_option.lower() == 'status':
                             edit_status = input('Digite o novo status (N, A, R): ')
@@ -158,22 +151,19 @@ class Toolbox():
                                 continue
                             task_processing = selected_task[2]
                             processing_node = self.processing_data_to_do(task_processing)
-                            update = self.update_to_do(processing_node, None, None, None, edit_status)
-                            print(f"\nStatus alterado para {edit_status}")
+                            update = self.update_to_do(processing_node, date_start, name_start, task_processing, edit_status)
+                            if update == 200:
+                                print(f"\nStatus alterado para {edit_status}")
                     
                     elif task_option == '2' or task_option.lower() == 'excluir':
                         list_date.remove(selected_task)
                         print(f"Tarefa '{selected_task[1]}' excluída com sucesso.")
-                    
                     else:
                         print("Opção inválida. Escolha 1 para editar ou 2 para excluir.")
-                
                 else:
                     print("Número de tarefa inválido.")
-            
             except ValueError:
                 print("Digite um número válido para a tarefa.")
-
         print("Saindo do menu de atualização de tarefas.")
 
 
@@ -186,9 +176,9 @@ class Toolbox():
 
 
     def weather( self, city):
-        API_KEY = '###'
+        api_key = self.key_weatherbit
         params = {
-            'key': API_KEY,
+            'key': api_key,
             'city': city,
             'lang': 'pt'
         }
@@ -212,23 +202,26 @@ class Toolbox():
 
             if option_main == '1' or option_main == 'criar':
                 date = input('Digite a data (dd-mm-aaaa): ')
+                if date == 'now':
+                    date = datetime.now().strftime('%d-%m-%Y')
                 if date == 'q':
                     self.toolbox()
                 name = input('Digite o nome: ')
+                print(name)
                 if name == 'q':
                     self.toolbox()
                 task = input('Digite a tarefa: ')
                 self.post_to_do(date, name, task)
 
             elif option_main == '2' or option_main == 'listar':
-                print('\nEscolha uma opção: \n 1 - Hoje \n 2 - Outra data')
+                print('\nEscolha uma opção: \n 1 - Hoje \n 2 - Outra data \n 3 - Listar tarefas não realizadas')
                 option_date = input('Digite o número da opção desejada: ')
 
                 if option_date == '1':
                     date_now = datetime.now().strftime('%d-%m-%Y')
                     list_date = self.list_to_do(date_now)
                     if not list_date:
-                        print('Sem tarefas para esta data.')
+                        continue
                     else:
                         self.menu_update_to_do(list_date)
 
@@ -238,9 +231,11 @@ class Toolbox():
                     if date_validate:
                         list_date = self.list_to_do(date_validate)
                         if not list_date:
-                            print('Sem tarefas para esta data.')
+                            continue
                         else:
                             self.menu_update_to_do(list_date)
+                elif option_date == '3':
+                    self.list_all_to_do()
 
                 elif option_date == 'q':
                     self.toolbox()
@@ -256,5 +251,8 @@ class Toolbox():
             else:
                 print('Opção inválida!')
 
-toolbox = Toolbox()
+url_firebase = 'SUA CONTA DO GOOGLE FIREBASE'
+waetherbit_key = 'SUA KEY DO WAETHERBIT'
+
+toolbox = Toolbox(url_firebase, waetherbit_key)
 main = toolbox.toolbox()
